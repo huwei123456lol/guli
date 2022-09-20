33abc0f8a5c747b840c4f65f83db05bae6a1bc8b
huwei123456lol <2281896839@qq.com>
2022-09-20 14:09:43 +0800

    Initial commit

---
+++ src/test/java/com/guli/example/guli_parent/GuliParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guli.example.guli_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
