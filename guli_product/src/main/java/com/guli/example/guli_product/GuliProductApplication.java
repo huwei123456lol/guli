fb39f28ef985c8ec9bf576116079776bbcdc0aa0
huwei123456lol <2281896839@qq.com>
2022-09-20 18:14:17 +0800

    添加了商品

---
+++ guli_product/src/main/java/com/guli/example/guli_product/GuliProductApplication.java
@@ -0,0 +1,13 @@
+package com.guli.example.guli_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliProductApplication.class, args);
+    }
+
+}
